84fb095394ec88942cfca3a5d4a0a0f2c4169399
Mominul Islam <mominulislam@graph-ai.com>
2021-06-30 04:55:02 +0600

    DOne Linear Search

--- linear_search.py
+++ linear_search.py
@@ -7,15 +7,47 @@
 3. if not found in array :
         print(not found)
 """
-
-def linear_search(list,findvalue):
+#Basic Function
+def BLinear_search(list,findvalue):
     for i in range(len(list)):
         if list[i]==findvalue:
-            print("Value Found at positon ",i)
+            print("Value Found  position at :  ",i)
             break
-  
-mList = [1,2,4,6,3,7,9,10]
-mfind = 9
-mSearch = linear_search(mList,mfind)
+    else:
+        print("Not Found ! ")
+#Boolean flag using FUnction
+def FLinear_search(mlist,findvalue):
+    found = False
+    for  i in range(len(mlist)):
+        if mlist[i] == findvalue:
+            found = True
+            positon = i
+            break
+    if found == True:
+        print("Value Found  position at :  ",positon)
+    else:
+        print("Value Not Found")
+
+#Function using Conditional loop
+def CLinear_search(mlist,findvalue):
+    found = False
+    count = 0
+    while count < len(mlist) and found == False:
+        if mlist[count] == findvalue:
+            found = True
+            postion = count
+            break
+        count +=1
+    if found == True:
+        print("Value Found  position at : ",postion)
+    else:
+        print("Value NOt FOund")
+
+mList =[int(x) for x in input("Enter The Value of List : ").split()]
+mfind = int(input("Enter the Find Value : "))
+
+mSearchB = BLinear_search(mList,mfind)
+mSearchF = FLinear_search(mList,mfind)
+mSearchC = CLinear_search(mList,mfind)
 #print(mSearch)
 
